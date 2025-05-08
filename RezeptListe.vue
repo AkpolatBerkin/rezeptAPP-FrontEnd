4b495231fdd6006e1f57df750071c00557fd8bed
AkpolatBerkin <berkinakpolat@gmail.com>
2025-05-08 14:45:52 +0200

    Initial commit mit Vue-App und RezeptListe-Komponente

---
+++ RezeptListe.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Rezept Liste</h2>
+    <ul>
+      <li v-for="rezept in rezepte" :key="rezept.id">
+        {{ rezept.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rezepte: [
+        { id: 1, name: 'Pasta Carbonara' },
+        { id: 2, name: 'Tomatensuppe' },
+        { id: 3, name: 'Pizza Margherita' }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+</style>
